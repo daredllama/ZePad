0b88ec01ecb9e734786d2dda3be4cf334e33037c
daredllama <prayagpoudel1@gmail.com>
2026-01-17 12:04:38 -0800

    Add files via upload

---
+++ keymap.c
@@ -0,0 +1,39 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+                        KC_ESC,
+        KC_Q,   KC_W,   KC_E,  
+        KC_A,   KC_S,   KC_D,   
+         
+    )
+};
+
+const uint16_t PROGMEM encoder_map[][1][2] = {
+    [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD) },
+};
+
+#ifdef OLED_ENABLE
+
+bool oled_task_user() {
+
+    oled_set_cursor(0,1)
+
+    oled_write("Zepad", false)
+    return false;
+}
+#endif
